40bb2989af038136d78f513d17457df23f191c97
Asger124 <asjo@itu.dk>
2025-02-16 16:25:39 +0100

    Added assignments up to 3.5

--- Assignments/Assignment3.fs
+++ Assignments/Assignment3.fs
@@ -9,15 +9,54 @@ module Assignment3
     let add5_2 f a = f a |> add5;;
     let mul3_2 f a = f a |> mul3;;
    
-    let rec downto4 _ = failwith "not implemented"
-                
-    let fac _ = failwith "not implemented"
+    // with pipes:
     
-    let range _ = failwith "not implemented"
+    let rec downto4 f n e = 
+        match n with 
+        |n when n <= 0 -> e  
+        |n ->  (f n e) |> downto4 f (n-1)
+
     
-    let rec double _ = failwith "not implemented"
+    (* Without: 
+
+    let rec downto4 f n e = 
+        match n with 
+        |n when n <= 0 -> e  
+        |n ->  downto4 f (n-1) (f n e)
+        *)
+    
+
+    let fac n = 
+        match n with 
+        |n when n=0 -> 1 
+        |n -> downto4 (fun acc x -> x*acc) n 1
+
+    (*
+    build a list with downto4. Construct a new list where g has been applied to all elements of list.
+    Done by sending the constructed list to List.map with piping
+    *)
+
+    let range g n = 
+        match n with 
+        |n when n<0 -> []
+        |n -> downto4 (fun x acc -> x::acc) n [] |> List.map g 
+
+    (*
+    Alternative version of the range function. 
+    Apply function g to each element before it is added to the constructed list.
+
+    let AlternativeRange g n = 
+        match n with 
+        |n when n<0 -> []
+        |n -> downto4 (fun x acc -> (g x)::acc) n [] 
+    *)
+    
+    let rec double xs  = 
+        match xs with 
+        |[] -> []
+        |x::xs -> (x*2)::double xs 
         
-    let double_2 _ = failwith "not implemented"
+    let double_2 lst = List.map (fun x -> x*2) lst 
     
     let rec stringLength _ = failwith "not implemented"
         
